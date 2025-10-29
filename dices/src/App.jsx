c942752be5a241e777bc5e2c48f955fb0227c9d4
Lshergela <lukagamingtv42@gmail.com>
2025-10-29 19:57:53 +0400

    das

--- dices/src/App.jsx
+++ dices/src/App.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import Player from "./components/Player";
 
 function App() {
-  const diceEmojis = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+  const [player1, setPlayer1] = useState(1);
+  const [player2, setPlayer2] = useState(1);
+  const [currentPlayer, setCurrentPlayer] = useState(1);
+
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+    if (currentPlayer === 1) {
+      setPlayer1(randomNumber);
+      setCurrentPlayer(2);
+    } else {
+      setPlayer2(randomNumber);
+      setCurrentPlayer(1);
+    }
+  };
 
   return (
     <div
@@ -14,11 +29,21 @@ function App() {
       <h1>2-Player Dice Game</h1>
 
       <div>
-        <Player title={"Player 1"} />
+        <Player
+          title={"Player 1"}
+          index={player1}
+          handleClick={rollDice}
+          isDisabled={currentPlayer === 2}
+        />
 
         <div>VS</div>
 
-        <Player title={"Player 2"} />
+        <Player
+          title={"Player 2"}
+          index={player2}
+          handleClick={rollDice}
+          isDisabled={currentPlayer === 1}
+        />
       </div>
     </div>
   );
--- dices/src/components/Player.jsx
+++ dices/src/components/Player.jsx
@@ -1,10 +1,14 @@
-export default function Player({ title }) {
+export default function Player({ title, index, handleClick, isDisabled }) {
+  const diceEmojis = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"];
+
   return (
     <>
       <div>
         <h3>{title}</h3>
-        <div style={{ fontSize: "60px" }}>⚀</div>
-        <p>1</p>
+        <div style={{ fontSize: "60px" }}>{diceEmojis[index - 1]}</div>
+        <button onClick={handleClick} disabled={isDisabled}>
+          Roll Dice
+        </button>
       </div>
     </>
   );
