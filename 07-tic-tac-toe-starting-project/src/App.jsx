cc6a4a1246c1b11597e1f2399fb50d633fb0d3f2
NikoZ21 <n.ambroladze@gmail.com>
2025-11-26 15:30:13 +0400

    1

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,18 @@
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+
 function App() {
-  return <h1>Tic-Tac-Toe Game</h1>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        <GameBoard />
+      </div>
+    </main>
+  );
 }
 
 export default App;
